510675f4e0ec9bf7c1d69c376e6693110e373aa9
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-06 18:35:01 +0500

    project settings added

---
+++ app/core/schemas/settings.py
@@ -0,0 +1,19 @@
+from functools import cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    PROJECT_DESCRIPTION: str
+    PROJECT_VERSION: str
+
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+def get_settings() -> Settings:
+    return Settings()
