7f91f6e7dc6fa5878c6e5601965a495e8ea6859c
Sobirjon005 <sobirjon0305@gmail.com>
2025-04-04 07:37:30 +0500

    updated

--- app/api/views/comments.py
+++ app/api/views/comments.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import Session
 from fastapi import APIRouter
+from app.api.models.comments import Comment
 
 router = APIRouter(
     prefix="/comments",
     tags=["comments"]
 )
 
+
+
 @router.get("/")
-async def read_comments():
-    pass
+async def read_comments(db: ):
+
 
 
