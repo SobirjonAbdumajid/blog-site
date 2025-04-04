3dab7674a0f060eeff21b809bf3a80356144bc11
Sobirjon005 <sobirjon0305@gmail.com>
2025-04-04 07:50:05 +0500

    updated

--- app/api/views/categories.py
+++ app/api/views/categories.py
@@ -1,25 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, HTTPException
 from app.api.models.categories import Category
-from typing import Annotated
-from sqlalchemy.orm import Session
-from app.api.views.auth import get_current_user
 from app.api.schemas.categories import CategoryBase
-from app.core.database.config import SessionLocal
 from app.api.views.posts import generate_slug
+from app.api.dependencies.dependencies import user_dependency, db_dependency
 
 router = APIRouter()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db_dependency = Annotated[Session, Depends(get_db)]
-user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/all/")
