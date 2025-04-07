095fbe6e0fb4727a19669094968f9d69eb3c60bd
Sobirjon <sobirjon0305@gmail.com>
2025-04-07 21:28:21 +0500

    modified

--- app/core/database/config.py
+++ app/core/database/config.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from app.core.settings import get_settings
+# from app.core.settings import get_settings
+from dotenv import load_dotenv
+import os
 
-settings = get_settings()
+# Load environment variables from .env file
+dotenv = load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.POSTGRES_USER}:\
-{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DB}'
+# settings = get_settings()
 
+SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("POSTGRES_USER", "postgres")}:{os.getenv("POSTGRES_PASSWORD", "123")}@{os.getenv("POSTGRES_HOST", "localhost")}:{os.getenv("POSTGRES_PORT", "5432")}/{os.getenv("POSTGRES_DB", "blog_db")}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
