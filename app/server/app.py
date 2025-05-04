a689b611af4af66d67d6da688bd803fba1cd23f0
Sobirjon <sobirjon0305@gmail.com>
2025-05-04 07:50:35 +0500

    Update server application configuration

--- app/server/app.py
+++ app/server/app.py
@@ -4,6 +4,7 @@ from app.api.views.users import router as users_router
 from app.api.views.auth import router as auth_router
 from app.api.views.test import router as posts_router
 from app.api.views.categories import router as categories_router
+from app.api.views.advisor import router as advisor_router
 
 settings = get_settings()
 
@@ -18,3 +19,4 @@ app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(auth_router, prefix='/auth', tags=["auth"])
 app.include_router(posts_router, prefix='/posts', tags=["posts"])
 app.include_router(categories_router, prefix='/categories', tags=["categories"])
+app.include_router(advisor_router, prefix='/advisor', tags=["advisor"])
