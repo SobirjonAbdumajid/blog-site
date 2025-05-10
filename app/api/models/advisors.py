8a00bbfa1547bbc85be5d842f23702f89e4a406e
Sobirjon <sobirjon0305@gmail.com>
2025-05-10 10:43:26 +0500

    Update Advisor model

--- app/api/models/advisors.py
+++ app/api/models/advisors.py
@@ -1,8 +1,10 @@
 from app.core.models.base import Base
-
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
 
 
 class Advisor(Base):
     __tablename__ = "advisors"
 
     advice_uzbek: Mapped[str] = mapped_column(String, nullable=False)
+    advice_english: Mapped[str] = mapped_column(String, nullable=False)
