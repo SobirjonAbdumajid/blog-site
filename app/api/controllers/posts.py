24aef476052572de13da124400be0143516fb36b
Sobirjon <sobirjon0305@gmail.com>
2025-05-16 06:18:54 +0500

    h

--- app/api/controllers/posts.py
+++ app/api/controllers/posts.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm import Session
 from app.api.repositories.posts import get_all_post_info, get_all_my_posts, get_all_post_info_with_slug
 
 
-def get_post_info(db: Session, skip: int, limit: int):
+async def get_post_info(db: Session, skip: int, limit: int):
     post = get_all_post_info(db, skip, limit)
     return post if post else []
 
 
 
-def get_my_posts(db: Session, user_id: int, skip: int, limit: int, post_status: str | None = None):
+async def get_my_posts(db: Session, user_id: int, skip: int, limit: int, post_status: str | None = None):
     """
     Fetches posts created by the current user.
     """
@@ -16,6 +16,6 @@ def get_my_posts(db: Session, user_id: int, skip: int, limit: int, post_status: 
     return my_posts if my_posts else []
 
 
-def get_post_info_with_slug(db: Session, slug: str, skip: int, limit: int):
+async def get_post_info_with_slug(db: Session, slug: str, skip: int, limit: int):
     post_with_slug = get_all_post_info_with_slug(db, slug, skip, limit)
     return post_with_slug if post_with_slug else []
