1a66cdb26b6b1a833d1b7e2768334406b7d6e6b5
Sobirjon <sobirjon0305@gmail.com>
2025-07-11 15:41:35 +0500

    modified

--- app/core/database/config.py
+++ app/core/database/config.py
@@ -6,7 +6,8 @@ from app.core.settings import get_settings
 
 settings = get_settings()
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}\
+@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
